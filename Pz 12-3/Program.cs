d6a793f393acb0c7930e2c15f6683311d6bf7764
is13126 <is13126@B206-2>
2023-01-19 08:28:48 +0700

    Добавьте файлы проекта.

---
+++ Pz 12-3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Pz_12_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            double A = Convert.ToDouble(Console.ReadLine());
+            double B = Convert.ToDouble(Console.ReadLine());
+            double x = Convert.ToDouble(Console.ReadLine());
+          
+            void root ()
+            {
+                double y = A * x + B;
+                if (B!=0)
+                {
+                    Console.WriteLine("B неравняется нулю");
+                }
+                else
+                {
+                    Console.WriteLine()ж ;
+                }
+                Console.WriteLine(y);
+            }
+        }
+    }
+}
